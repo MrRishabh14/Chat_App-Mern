46b58379fa8d1f2502ecfd87ec4a25f2a5197f33
Rishabh Puthran <rishabhputhran142004@gmail.com>
2025-08-08 23:17:25 +0530

    auth middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,16 @@
+import User from "../models/User.model";
+import jwt from "jsonwebtoken"
+export const protectRoute=async(req,res,next)=>{
+  try{
+    const token=req.headers.token;
+    const decoded=jwt.verify(token,process.env.JWT_SCERET);
+    const user= await User.findById(decoded.userId).select("-password");
+    if(!user) return res.json({sucess:false,message:"User not found"});
+    req.user=user;
+    next();
+  }
+  catch(e){
+    console.log(e.message);
+    return res.json({sucess:false,message:e.message});
+  }
+}
